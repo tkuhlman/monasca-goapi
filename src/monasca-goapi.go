9decb5c3c48f1f3888b5a0db0fdd9769b4c21fdf
Tim Kuhlman <tim.kuhlman@hp.com>
2014-08-14 08:33:05 -0600

    Turned on kafka

--- src/monasca-goapi.go
+++ src/monasca-goapi.go
@@ -33,48 +33,11 @@ func (m metric) Encode() ([]byte, error) {
 	return buffer.Bytes(), nil
 }
 
-func main() {
-	runtime.GOMAXPROCS(runtime.NumCPU()) // Use all the machine's cores
-
-	kafkaHosts := []string{"192.168.10.4:9092"}
-	//	kafkaHosts := []string{"10.22.156.14:9092", "10.22.156.15:9092", "10.22.156.16:9092"}
-	measurements := make(chan metric, 1024) // Room for 1024 as buffer to spikes
-
-	go kafkaProducer(kafkaHosts, measurements)
-
-	// todo I need to figure out how to pass in measurements chan to the handleFunc
-	http.HandleFunc("/v2.0/metrics", metrics)
-	if err := http.ListenAndServe(":8000", nil); err != nil {
-		log.Fatal("failed to start server", err)
-	}
+type metricsHandler struct {
+	measurements chan<- metric
 }
 
-// Produce to kafka anything read from the measurements channel
-func kafkaProducer(url []string, measurements <-chan metric) {
-
-	//Setup the producer
-	client, err := sarama.NewClient("goapi", url, sarama.NewClientConfig())
-	if err != nil {
-		panic(err)
-	}
-	defer client.Close()
-	producer, err := sarama.NewProducer(client, nil)
-	if err != nil {
-		panic(err)
-	}
-	defer producer.Close()
-
-	//Push from the measurements channel to kafka
-	for measurement := range measurements {
-		// todo I should check for errors coming back from Kafka
-		err = producer.QueueMessage("message", nil, measurement)
-		if err != nil {
-			fmt.Printf("Unable to publish to Kafka\n\t%v", err)
-		}
-	}
-}
-
-func metrics(writer http.ResponseWriter, request *http.Request) {
+func (mh metricsHandler) ServeHTTP(writer http.ResponseWriter, request *http.Request) {
 	bodyBuffer := new(bytes.Buffer)
 	_, err := bodyBuffer.ReadFrom(request.Body)
 	if err != nil && err != io.EOF {
@@ -94,9 +57,48 @@ func metrics(writer http.ResponseWriter, request *http.Request) {
 	}
 
 	for _, measurement := range metrics {
-		fmt.Printf("%+v", measurement)
-		//		measurements <- measurement
+		mh.measurements <- measurement
 	}
 
 	writer.WriteHeader(http.StatusNoContent) //StatusNoContent == 204
+}
+
+func main() {
+	runtime.GOMAXPROCS(runtime.NumCPU()) // Use all the machine's cores
+
+	kafkaHosts := []string{"192.168.10.4:9092"}
+	//	kafkaHosts := []string{"10.22.156.14:9092", "10.22.156.15:9092", "10.22.156.16:9092"}
+	measurements := make(chan metric, 1024) // Room for 1024 as buffer to spikes
+
+	go kafkaProducer(kafkaHosts, measurements)
+
+	handler := metricsHandler{measurements} // Should only respond on /v2.0/metrics but as it is the only functionality I let it respond everwhere
+	if err := http.ListenAndServe(":8000", handler); err != nil {
+		log.Fatal("failed to start server", err)
+	}
+}
+
+// Produce to kafka anything read from the measurements channel
+func kafkaProducer(url []string, measurements <-chan metric) {
+
+	//Setup the producer
+	client, err := sarama.NewClient("goapi", url, sarama.NewClientConfig())
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer client.Close()
+	producer, err := sarama.NewProducer(client, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer producer.Close()
+
+	//Push from the measurements channel to kafka
+	for measurement := range measurements {
+		// todo I should check for errors coming back from Kafka
+		err = producer.QueueMessage("message", nil, measurement)
+		if err != nil {
+			fmt.Printf("Unable to publish to Kafka\n\t%v", err)
+		}
+	}
 }
